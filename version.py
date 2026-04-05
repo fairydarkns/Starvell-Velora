496d3f078296a578378a5cf36087ce15cf1f0adf
fairydark <godboygobdoy@gmail.com>
2026-04-05 22:34:07 +0300

    Апдейт версии

--- version.py
+++ version.py
@@ -1,6 +1,6 @@
 """Runtime version metadata for manual updates."""
 
-VERSION = "0.0.7"
+VERSION = "0.0.8"
 GITHUB_REPO = "fairydarkns/Starvell-Velora"
 UPDATE_BRANCH = "master"
 VERSION_URL = f"https://raw.githubusercontent.com/{GITHUB_REPO}/{UPDATE_BRANCH}/version.py"
